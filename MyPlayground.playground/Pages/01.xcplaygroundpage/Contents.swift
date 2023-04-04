9c7fb6d4ec90254072aaf3eb35c4d2f09fbd016c
yiminprogram <minwork1016@outlook.com>
2023-04-04 23:07:23 +0800

    add variable print

---
+++ MyPlayground.playground/Pages/01.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//變數，可變動
+var apple:String = "apple"
+
+// 變數，不可變動
+let banana:String = "banana"
+
+// 按住option可看到內容
+
+// 列印
+print("Hello",apple)
+print("Hello",apple,separator:"---")
+print("Hello",apple,separator:"---",terminator:"!!!\n")
